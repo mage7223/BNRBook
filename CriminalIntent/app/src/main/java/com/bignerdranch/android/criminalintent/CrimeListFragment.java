59a89e8301fcbe29a97e53aab4f352ffda6c6ecb
Kevin Rogers <kevin@KevinRogers.org>
2016-03-24 01:26:51 -0400

    Worked into chapter 9 and added some abstract classes, refactored existing classes to use them, added a new mail activity by modifying the manifest

---
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -0,0 +1,11 @@
+package com.bignerdranch.android.criminalintent;
+
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by krogers on 3/24/16.
+ */
+public class CrimeListFragment extends Fragment {
+    // Nothing Yet
+}
