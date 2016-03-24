d3fa506bdaa4e9336dce08f2a82d5c347494f0df
Kevin Rogers <kevin@KevinRogers.org>
2016-03-24 00:27:42 -0400

    Done with Chapter 8

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -1,5 +1,6 @@
 package com.bignerdranch.android.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -8,10 +9,13 @@ import java.util.UUID;
 public class Crime {
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
 
     public Crime() {
         // Generate unique identifier
         mId = UUID.randomUUID();
+        this.mDate = new Date();
     }
 
     public UUID getId() {
@@ -24,5 +28,21 @@ public class Crime {
 
     public void setTitle(String title){
         mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date mDate) {
+        this.mDate = mDate;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean mSolved) {
+        this.mSolved = mSolved;
     }
 }
