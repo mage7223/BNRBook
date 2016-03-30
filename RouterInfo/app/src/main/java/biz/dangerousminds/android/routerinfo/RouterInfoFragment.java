dec329493df62370a5970e2735a5631d6ae82726
Kevin Rogers <kevin@KevinRogers.org>
2016-03-30 12:07:13 -0400

    Initial commit of RouterInfo. New project with no mods

---
+++ RouterInfo/app/src/main/java/biz/dangerousminds/android/routerinfo/RouterInfoFragment.java
@@ -0,0 +1,22 @@
+package biz.dangerousminds.android.routerinfo;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class RouterInfoFragment extends Fragment {
+
+    public RouterInfoFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_router_info, container, false);
+    }
+}
